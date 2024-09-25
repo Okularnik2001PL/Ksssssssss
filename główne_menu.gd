982baeb194511370120909fb5334818b56ffacea
Okularnik2001PL <44050332+Okularnik2001PL@users.noreply.github.com>
2024-09-25 17:38:35 +0200

    dodatkowe
    sciongnij

--- główne_menu.gd
+++ główne_menu.gd
@@ -8,3 +8,6 @@ func _on_button_button_down():
 func _on_button_2_button_down():
 	get_tree().change_scene_to_file("res://oku_menu.tscn")
 
+
+func _on_button_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://walka.tscn")
