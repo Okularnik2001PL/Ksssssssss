3adc020aa8cacf07cb5a673501e222894ecd0212
Okularnik2001PL <44050332+Okularnik2001PL@users.noreply.github.com>
2024-09-26 17:51:44 +0200

    yyyyy
    yyyyyy

--- Nowe/scrypty/BronieEQ.gd
+++ Nowe/scrypty/BronieEQ.gd
@@ -1,5 +1,28 @@
 extends Node2D
-var Posiadane=["Miecz","Siekiera"]
+
+var Posiadane = ["Miecz", "Siekiera"]
+var file_path = "res://tabelki/bron_statystki.csv"
+
 func _ready() -> void:
-	for i in Posiadane.size():
+	# Dodawanie przedmiotów do ItemList
+	for i in range(Posiadane.size()):
 		$ItemList.add_item(Posiadane[i])
+
+	# Tworzenie instancji klasy FileAccess
+	var file = FileAccess.open(file_path, FileAccess.READ)
+	
+	if file:
+		# Odczytanie całej zawartości pliku
+		var content = file.get_buffer(file.get_length()).get_string_from_utf8()
+
+		# Podział na linie
+		var lines = content.split("\n")
+
+		# Iteracja przez linie i ich podział na kolumny (zakładamy, że dane są rozdzielone przecinkami)
+		for line in lines:
+			var columns = line.split(",")
+			print(columns)  # Wyświetlenie danych z każdej kolumny
+
+		# Zamknięcie pliku (FileAccess automatycznie zamyka plik po zakończeniu)
+	else:
+		print("Nie można otworzyć pliku:", file_path)
