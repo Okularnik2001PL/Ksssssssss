e59d7a3b1fe2d2ceb5e7d9b85ddcd6450d1275c2
Okularnik2001PL <44050332+Okularnik2001PL@users.noreply.github.com>
2024-10-22 18:22:35 +0200

    z tego scryptu jestem dumny tak powinny wyglądać wszstkie

--- Nowe/scrypty/wyposażenie.gd
+++ Nowe/scrypty/wyposażenie.gd
@@ -2,9 +2,16 @@ extends Node2D
 var file_path = "res://zapis/EQ_gracz/bron_EQ_gracz.csv"
 var file_path2= "res://tabelki/bron_statystki.csv"
 func _ready() -> void:
+	wczytaj("res://tabelki/bron_statystki.csv","res://zapis/EQ_gracz/bron_EQ_gracz.csv",$"VBoxContainer/HBoxContainer/Ręka1")
+	wczytaj("res://tabelki/buty_statystyki.csv","res://zapis/EQ_gracz/buty_EQ_moje.csv",$VBoxContainer/Stopy)
+	wczytaj("res://tabelki/helmy_statystyki.csv","res://zapis/EQ_gracz/helmy_EQ_gracz.csv",$"VBoxContainer/Głowa")
+	wczytaj("res://tabelki/spodnie_statystki.csv","res://zapis/EQ_gracz/spodnie_EQ_gracz.csv",$VBoxContainer/Nogi)
+	wczytaj("res://tabelki/tarcze_statystki.csv","res://zapis/EQ_gracz/tarcze_EQ_gracz.csv",$"VBoxContainer/HBoxContainer/Ręka2")
+	wczytaj("res://tabelki/zbroje_statystyki.csv","res://zapis/EQ_gracz/zbroje_EQ_gracz.csv",$VBoxContainer/HBoxContainer/Tors)
+func wczytaj(dane,magazyn,do):
 	# Otwieranie pliku w trybie zapisu z dodawaniem danych (READ_WRITE)
-	var file = FileAccess.open(file_path, FileAccess.READ_WRITE)
-	var file2 = FileAccess.open(file_path2, FileAccess.READ)
+	var file = FileAccess.open(magazyn, FileAccess.READ_WRITE)
+	var file2 = FileAccess.open(dane, FileAccess.READ)
 	if file && file2:
 		# Odczytanie zawartości pliku
 		var content = file.get_buffer(file.get_length()).get_string_from_utf8()
@@ -20,10 +27,8 @@ func _ready() -> void:
 					if columns[0]!="EQ"&&columns.size() > 1:
 						if columns2[0]!="ID":
 							if columns2[0]==columns[1]:
-								$"VBoxContainer/HBoxContainer/Ręka1".text=columns2[1]
+								do.text=columns2[1]
 								print(columns)
-
-
 func _on_głowa_pressed() -> void:
 	get_tree().change_scene_to_file("res://Nowe/Sceny/hełmy.tscn")
 
